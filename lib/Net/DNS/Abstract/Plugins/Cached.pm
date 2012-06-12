ba4048c31be7f664ff2e0d4e8ce32a5f0e8fe010
Lenz Gschwendtner <lenz@springtimesoft.com>
2012-06-12 14:10:34 +1200

    some more record type parsing

--- lib/Net/DNS/Abstract/Plugins/Cached.pm
+++ lib/Net/DNS/Abstract/Plugins/Cached.pm
@@ -3,17 +3,17 @@ package Net::DNS::Abstract::Plugins::Cached;
 use 5.010;
 use Any::Moose;
 use True::Truth;
-use Net::DNS;
+use Net::DNS::Update;
+use Data::Dumper;
 
 extends 'Net::DNS::Abstract';
 
 has 'truth' => (
     is      => 'ro',
     isa     => 'True::Truth',
-    default => sub{ True::Truth->new() },
+    default => sub { True::Truth->new() },
     lazy    => 1,
 );
-
 
 =head2 register
 
@@ -27,7 +27,6 @@ sub provides {
     return { Cached => { axfr => \&status_zone } };
 }
 
-
 =head2 status_zone
 
 Query a DNS zone via InternetX
@@ -35,16 +34,103 @@ Query a DNS zone via InternetX
 =cut
 
 sub status_zone {
-    my($self, $domain) = @_;
+    my ($self, $domain) = @_;
 
     my $truth = $self->truth->get_true_truth($domain);
-    my @rr;
-    foreach my $rr (@{$truth->{dns}->{rr}}){
-        push(@rr, Net::DNS::RR->new($rr));
+    my $dns = Net::DNS::Update->new($domain, 'IN');
+
+    print Dumper($truth->{dns});
+    $dns->push(
+        update => Net::DNS::RR->new(
+            name    => $domain,
+            retry   => $truth->{dns}->{soa}->{retry},
+            rname   => $truth->{dns}->{soa}->{email},
+            refresh => $truth->{dns}->{soa}->{refresh},
+            ttl     => $truth->{dns}->{soa}->{ttl},
+            expire  => $truth->{dns}->{soa}->{expire},
+            type    => 'SOA',
+        ));
+    foreach my $rr (@{ $truth->{dns}->{rr} }) {
+        given ($rr->{type}) {
+            when (/^a*$/i) {
+                $dns->push(
+                    update => Net::DNS::RR->new(
+                        name => (
+                            $rr->{name} ? $rr->{name} . '.' . $domain : $domain
+                        ),
+                        class   => 'IN',
+                        ttl     => $rr->{ttl},
+                        type    => $rr->{type},
+                        address => $rr->{value},
+                    ));
+            }
+            when (/^cname$/i) {
+                $dns->push(
+                    update => Net::DNS::RR->new(
+                        name => (
+                            $rr->{name} ? $rr->{name} . '.' . $domain : $domain
+                        ),
+                        class => 'IN',
+                        ttl   => $rr->{ttl},
+                        type  => $rr->{type},
+                        cname => $rr->{value},
+                    ));
+            }
+            when (/^mx$/i) {
+                $dns->push(
+                    update => Net::DNS::RR->new(
+                        name => (
+                            $rr->{name} ? $rr->{name} . '.' . $domain : $domain
+                        ),
+                        class    => 'IN',
+                        ttl      => $rr->{ttl},
+                        type     => $rr->{type},
+                        exchange => $rr->{value},
+                        prio     => $rr->{prio},
+                    ));
+            }
+            when (/^srv$/i) {
+                my ($weight, $port, $target) = split(/\s/, $rr->{value}, 3);
+                $dns->push(
+                    update => Net::DNS::RR->new(
+                        name => (
+                            $rr->{name} ? $rr->{name} . '.' . $domain : $domain
+                        ),
+                        class  => 'IN',
+                        ttl    => $rr->{ttl},
+                        type   => $rr->{type},
+                        target => $target,
+                        weight => $weight,
+                        port   => $port,
+                        prio   => $rr->{prio},
+                    ));
+            }
+            when (/^txt$/i) {
+                $dns->push(
+                    update => Net::DNS::RR->new(
+                        name => (
+                            $rr->{name} ? $rr->{name} . '.' . $domain : $domain
+                        ),
+                        class   => 'IN',
+                        ttl     => $rr->{ttl},
+                        type    => $rr->{type},
+                        txtdata => $rr->{value},
+                    ));
+            }
+        }
+    }
+    foreach my $rr (@{ $truth->{dns}->{ns} }) {
+        $dns->push(
+            update => Net::DNS::RR->new(
+                name    => $rr->{name},
+                class   => 'IN',
+                type    => 'NS',
+                address => $rr->{ip} || undef,
+            ));
     }
 
-    $truth->{dns}->{rr} = \@rr;
-    return $truth->{dns};
+    print Dumper($dns);
+    return $dns;
 }
 
 __PACKAGE__->meta->make_immutable();
---
+++ t/cached.t
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+use Test::More;
+use Test::Deep;
+use lib 'lib';
+
+plan qw/no_plan/;
+
+{
+
+    BEGIN { use_ok('Net::DNS::Abstract'); }
+    my $dns = Net::DNS::Abstract->new();
+    ok($dns);
+
+    my $a = { domain => 'domain.tld', interface => 'Cached' };
+    my $b = $dns->axfr($a);
+    ok($b);
+    cmp_deeply(
+        $b, {
+            domain => 'example.com',
+            interface => 'Cached',
+        },
+        "check hash structure for Cached"
+    );
+    print Dumper $b;
+
+}
+
